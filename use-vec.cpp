89c4414c2ca53a3becc4ed5d14429aaa0504b2fc
Karl Yerkes <karl.yerkes@gmail.com>
2024-01-25 06:35:14 -0800

    arithmetic on vec3f

--- use-vec.cpp
+++ use-vec.cpp
@@ -7,6 +7,13 @@ int main() {
   std::cout << al::dot(a, b) << std::endl;
   std::cout << al::cross(a, b) << std::endl;
   std::cout << al::lerp(a, b, 0.5) << std::endl;
+
+  float t = 0.3333;
+  std::cout << a * (1 - t) + b * t << std::endl;
+  //std::cout << a * (1 - t) + t * b << std::endl; // does not work
+
+  // * / + - ... these work on vec3f
+
   al::rotate(a, b, M_PI_4);
   std::cout << a << std::endl;
 }
